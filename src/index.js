35ac3c26605c85fe1fd7d3ffccb5ab8ed4519714
风之忧伤 <575792372@qq.com>
2021-11-18 11:05:56 +0800

    提交最新的代码

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
+// 测试加载成功方法
+export function loadSuccess() {
+  console.log("[lemon-tools] loaded successfully");
+}
+
 // export { default as string } from "./string";
 // export { default as array } from "./array";
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,17 @@ const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 module.exports = {
   mode: "production",
   entry: {
+    // 压缩文件入口
     "lemon-tools": path.resolve(__dirname, "./src/index.js"),
     "lemon-tools.min": path.resolve(__dirname, "./src/index.js"),
   },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
-    library: "LemonTools",
+    library: "LemonTools", // 工具使用的变量名
     libraryTarget: "umd",
   },
+  // 压缩文件插件
   optimization: {
     minimize: true,
     minimizer: [
